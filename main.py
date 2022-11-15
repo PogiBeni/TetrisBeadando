d543e93c9862e167f30203a87ff10def57a13dcc
PoganyBenedek <poganybenedek@gmail.com>
2022-11-15 12:42:51 +0100

    yes
    yes

--- main.py
+++ main.py
@@ -7,13 +7,9 @@ from random import choice, randrange
 Sz,M = 10,20
 Csempe = 40
 GAME_RES = Sz * Csempe, (M + 1) * Csempe
-############################################
 FPS = 60
-
 pygame.init()
-
 pygame.display.set_caption('Tetris')
-#Ez az ablak ami megnyílik egy változó ként
 GAME = pygame.display.set_mode(GAME_RES)
 clock = pygame.time.Clock()
 font = pygame.font.Font('font.ttf', Csempe-10)
@@ -29,29 +25,25 @@ FigurakKoordinatai = [  [(-1, 0), (-2, 0), (0, 0), (1, 0)],
 Figurak = [[pygame.Rect(x + Sz // 2, y + 1, 1, 1) for x, y in FiguraHelyzet] for FiguraHelyzet in FigurakKoordinatai]
 FiguraNegyzet = pygame.Rect(0,0, Csempe - 2, Csempe - 2)
 Mezo = [[0 for i in range(Sz)] for j in range (M)]
+
 animSzam, animSebesseg, animLimit = 0 ,8, 2000
-Figura =  deepcopy(choice(Figurak))
-
-
 Pont = 0
 Pontok = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
 getColor = lambda : (randrange(30,256), randrange(30,256), randrange(30,256) )
 color = getColor()
-
+Figura =  deepcopy(choice(Figurak))
 
 
 #A grid létrehozása változóként, (x = oszop, y = sor, Szelesség, Magasság)
 grid = [pygame.Rect(x * Csempe, y * Csempe, Csempe, Csempe) for x in range(Sz) for y in range(M)]   
+PontokText = font.render('Pontok:', True, getColor())
 
-def hitboxChech():
+def hitboxCheck():
     if Figura[i].x < 0 or Figura[i].x > Sz - 1:
         return False
     elif Figura[i].y > M - 1 or Mezo[Figura[i].y][Figura[i].x]:
         return False
     return True
-
-
-PontokText = font.render('Pontok:', True, getColor())
 
 while True:
     mozgasX, forgass = 0, False
@@ -72,7 +64,7 @@ while True:
     regi_Figura = deepcopy(Figura)
     for i in range(4):
         Figura[i].x += mozgasX
-        if not hitboxChech():
+        if not hitboxCheck():
             Figura = deepcopy(regi_Figura)
             break
     # Y tengely mozgás
@@ -82,7 +74,7 @@ while True:
         regi_Figura = deepcopy(Figura)
         for i in range(4):
             Figura[i].y += 1
-            if not hitboxChech():
+            if not hitboxCheck():
                 for i in range(4):
                    Mezo[regi_Figura[i].y][regi_Figura[i].x] = color
                 color = getColor()
@@ -99,9 +91,10 @@ while True:
             y = Figura[i].x - forgoPont.x
             Figura[i].x = forgoPont.x - x
             Figura[i].y = forgoPont.y + y
-            if not hitboxChech():
+            if not hitboxCheck():
                 Figura = deepcopy(regi_Figura)
                 break
+
     # Egész sor törlése
     vonal,vonalak = M-1, 0
     for sor in range(M -1,-1,-1):
@@ -114,11 +107,11 @@ while True:
             vonal -=1
         else:
             vonalak += 1
-    
     Pont += Pontok[vonalak]
-    #A grid kirajzolása: (Kijelző, (RGB szinek), i az maga a létrehozott grid ek tulajdonságai, milyne vastag a border)
-    [pygame.draw.rect(GAME, (50,50,50), i , 1) for i in grid]         
 
+    #A grid kirajzolása: (Kijelző, (RGB szinek), i az maga a létrehozott grid ek tulajdonságai, milyne vastag a border)
+
+    [pygame.draw.rect(GAME, (50,50,50), i , 1) for i in grid]         
     #Jelenleg eső figura rajzolása
     for i in range(4):
         FiguraNegyzet.x = Figura[i].x * Csempe
